acabdd0fe3180de044c67d8846df83705944f93e
Ranjeet Dethe <130579549+ranjeetdethe@users.noreply.github.com>
2024-08-21 23:04:49 +0530

    Add files via upload

---
+++ Program8.dart
@@ -0,0 +1,11 @@
+void main(){
+  int mul=1;
+  int i=10;
+  while(i>=1){
+    if(i%2==1){
+      mul=mul*i;
+    }
+  }
+  print(mul);
+  i++;
+}
---
+++ Program9.dart
@@ -0,0 +1,20 @@
+void main(){
+  int days=7;
+  while(days>=0){
+    if(days==0){
+      print("$days  assignment is overdue");
+    }
+    else if(days==1){
+      print("$days days is remaining");
+      
+    }
+    else if(days==1){
+      print("$days days is remaining");
+      
+    }
+    else{
+      print("$days days remaining");
+    }
+    days--;
+  }
+}
